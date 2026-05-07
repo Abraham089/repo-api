40c467211e6bd4a68486170e77b943e4992fd9e9
Abraham089 <abrahamgb274@gmail.com>
2026-05-07 10:07:29 -0600

    Endpoint creado

--- src/repo_api_equipo_e/models/woo.py
+++ src/repo_api_equipo_e/models/woo.py
@@ -2,9 +2,37 @@ from pydantic import BaseModel, Field
 from typing import List
 
 class BillingInfo(BaseModel):
-    first_name: str = Field(min_length=1)
-    last_name: str = Field(min_length=1)
-    email: str
+   address_1: str = Field(min_length=1)
+   city: str = Field(min_length=1)
+   country: str = Field(min_length=1)
+   model_config = {
+     "json_schema_extra": {
+        "example": {
+          "billing": {
+              "address_1": "354 calle de ejemplo",
+              "city": "ciudad de ejemplo",
+              "country": "pais de ejemplo"
+              }
+          }
+      }
+    }
+
+class ShippingInfo(BaseModel):
+   address_1: str = Field(min_length=1)
+   city: str = Field(min_length=1)
+   country: str = Field(min_length=1)
+
+   model_config = {
+     "json_schema_extra": {
+        "example": {
+          "shipping": {
+              "address_1": "354 calle de ejemplo",
+              "city": "ciudad de ejemplo",
+              "country": "pais de ejemplo"
+              }
+          }
+      }
+    }
 
 class LineItem(BaseModel):
     product_id: int = Field(gt=0)
@@ -29,6 +57,33 @@ class RequestedOrder(BaseModel):
               "email": "alguien@ejemplo.com"
               },
           "line_items": [{"product_id": 1, "quantity": 1}]
+          }
+      }
+    }
+
+class RequestedCustomer(BaseModel):
+  first_name: str = Field(min_length=1)
+  last_name: str = Field(min_length=1)
+  email: str = Field(min_length=1)
+  billing: BillingInfo
+  shipping: ShippingInfo
+
+  model_config = {
+     "json_schema_extra": {
+        "example": {
+          "first_name": "nombre de ejemplo",
+          "last_name": "apellido de ejemplo",
+          "email": "ejemplo@algo.com",
+          "billing": {
+              "address_1": "354 calle de ejemplo",
+              "city": "ciudad de ejemplo",
+              "country": "pais de ejemplo"
+              },
+          "shipping": {
+              "address_1": "354 calle de ejemplo",
+              "city": "ciudad de ejemplo",
+              "country": "pais de ejemplo"
+              }
           }
       }
     }
