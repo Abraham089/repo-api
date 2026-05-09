72a0ff41d393ba0379ee4f0728cd618b5eeff3c5
ᓚᘏᗢ !? <datamayo.211@gmail.com>
2026-05-09 11:49:14 -0600

    Endpoint creado

--- src/repo_api_equipo_e/routers/WooCommerce/getCuponsById.py
+++ src/repo_api_equipo_e/routers/WooCommerce/getCuponsById.py
@@ -17,3 +17,16 @@ wcapi = API(
     version="wc/v3",
     timeout=20
 )
+
+@router.get("/coupons/{coupon_id}")
+def get_coupon_by_id(coupon_id: int):
+  response = wcapi.get(f"cupons/{coupon_id}")
+
+  if response.status_code == 200:
+    print(f"ID: {response.json()['id']} | Tipo: {response.json()['type']} | Importe: ${response.json()['amount']} | Descripción: {response.json()['description']}")
+
+  else:
+    print(f"Error {response.status_code}: {response.text}")
+
+
+  return response.json()
